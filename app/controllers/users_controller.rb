49fcbefd2d0d2728d7d00c3245210f120c0e4ea5
mattyc246 <matty_c_246@gmail.com>
2018-10-23 17:14:02 +0800

    permit params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,30 @@ class UsersController < ApplicationController
 
 	def sign_up
 
-		
+		user = User.new()
 
 	end
 
 	def sign_in
 
+		user = User.find(params[:email])
+
+		if params[:password] == user.password
+
+			session[:user_id] = user.id
+			flash[:notice] = "Successfully Logged In!"
+			redirect_to root_path
+
+		end
+
 	end
 
 	private
 
+	def user_params
+
+		params.require(:user).permit(:first_name,:last_name,:email,:password,:address,:city,:state,:postcode,:country,:phone_number,:photo)
+
+	end
 
 end
