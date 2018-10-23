9fb107ea642833634c8679cf81cb2ac5188c4a07
mattyc246 <matty_c_246@gmail.com>
2018-10-23 15:38:19 +0800

    postings migration file completed

--- db/migrate/20181023062910_create_postings.rb
+++ db/migrate/20181023062910_create_postings.rb
@@ -1,8 +1,18 @@
 class CreatePostings < ActiveRecord::Migration[5.2]
   def change
     create_table :postings do |t|
-
+      t.string :title
+      t.string :brand
+      t.string :type
+      t.string :style
+      t.integer :size
+      t.string :color
+      t.integer :condition
+      t.string :expected_offer
+      t.belongs_to :user
+      t.string :photos
       t.timestamps
     end
   end
 end
+
