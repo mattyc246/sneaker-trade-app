fb89e2419681228546946c12b0c7d21193fbd0e3
mattyc246 <matty_c_246@gmail.com>
2018-10-23 15:55:23 +0800

    routes for sign up & sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
+
+  get '/sign_up' => 'users#sign_up', as: 'sign_up'
+  get '/sign_in' => 'users#sign_in', as: 'sign_in'
+  
+  resources :users
+
+  resources :postings do
+  	resource :trades
+  end
   
 end
