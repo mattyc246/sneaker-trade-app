516bc04e7abbee59b0692495dcd229d178742200
mattyc246 <matty_c_246@gmail.com>
2018-10-23 15:33:09 +0800

    add carrierwave gem to handle photo uploads

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+# Use carrierwave to handle photo uploads
+gem 'carrierwave', '~> 1.0'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- db/migrate/20181023062833_create_users.rb
+++ db/migrate/20181023062833_create_users.rb
@@ -1,8 +1,30 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :postcode
+      t.string :country
+      t.string :phone_number
+      
       t.timestamps
     end
   end
 end
+
+Users:
+- First Name
+- Last Name
+- Email
+- Password (Password Digest - bcrypt)
+- Address
+- State
+- Postcode
+- Country
+- Phone Number
+- Profile Photo
