072e7bd25e50142d2b900285a92960ec31d4e434
mattyc246 <matty_c_246@gmail.com>
2018-10-23 16:08:32 +0800

    created sign up & sign in function inside of user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+	def index
+
+	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
+	def delete
+
+	end
+
+	def sign_up
+
+	end
+
+	def sign_in
+
+	end
+
+	private
+
+
 end
