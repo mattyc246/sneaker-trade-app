16c20158496654bbc07fb0ef588e6dc013e67a9a
mattyc246 <matty_c_246@gmail.com>
2018-10-23 15:40:28 +0800

    trade migration file completed

--- db/migrate/20181023062943_create_trades.rb
+++ db/migrate/20181023062943_create_trades.rb
@@ -1,7 +1,10 @@
 class CreateTrades < ActiveRecord::Migration[5.2]
   def change
     create_table :trades do |t|
-
+      t.belongs_to :user
+      t.belongs_to :posting
+      t.string :offer
+      t.boolean :offer_status
       t.timestamps
     end
   end
