b87b33e8c46ff175009cd324f0be841b2c3e62ef
mattyc246 <matty_c_246@gmail.com>
2018-10-24 10:28:48 +0800

    posting params & new method created

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -1,2 +1,16 @@
 class PostingsController < ApplicationController
+
+	def new
+
+	end
+
+
+	private
+
+	def posting_params
+
+		params.require(:posting).permit(:title, :brand, :type, :style, :size, :color, :condition, :expected_offer, :photos)
+
+	end
+
 end
