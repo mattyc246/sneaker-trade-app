946ecb57115713a7e856e68c17b6a909f630b018
mattyc246 <matty_c_246@gmail.com>
2018-10-27 22:52:46 +0800

    helper method created to check for superadmin level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-	  helper_method :current_user, :logged_in?
+	  helper_method :current_user, :logged_in?, :superadmin?
 
 	  def current_user
 	    @current_user ||= User.find_by(id: session[:user_id])
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
 
 	  def logged_in?
 	    current_user != nil
+	  end
+
+	  def superadmin?
+	  	current_user.user_level == "superadmin"
 	  end
 	
 
